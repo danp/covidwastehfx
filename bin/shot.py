9e3f06cf1426c9f7162c917246222ec3df181972
Dan Peterson <danp@danp.net>
2024-10-03 19:38:44 -0300

    split into one image per chart

--- bin/shot.py
+++ bin/shot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from time import sleep
 from playwright.sync_api import sync_playwright, expect
 
 def main(args):
@@ -11,10 +12,14 @@ def main(args):
         page = context.new_page()
         page.goto("https://health-infobase.canada.ca/wastewater/")
         page.get_by_label("Select a province/territory:").select_option("Nova Scotia")
+        sleep(1) # ???
         page.get_by_label("Select a municipality:").select_option("Halifax")
         locator = page.locator("#smallmultiples-container")
-        expect(locator.locator("div")).to_have_count(count=3, timeout=10000)
-        locator.screenshot(path=path)
+        divs = locator.locator("div")
+        expect(divs).to_have_count(count=3, timeout=10000)
+        for idx, div in enumerate(divs.all()):
+            p = f"{path}{idx}"
+            div.screenshot(path=p)
         browser.close()
 
 if __name__ == "__main__":
