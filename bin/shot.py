052ddd98975c5f32134d6e7bcbdeb4e8ede798e3
Dan Peterson <danp@danp.net>
2024-10-06 15:32:40 -0300

    improve post and alt text

--- bin/shot.py
+++ bin/shot.py
@@ -5,7 +5,8 @@ from time import sleep
 from playwright.sync_api import sync_playwright, expect
 
 def main(args):
-    path = args[0]
+    date = args[0]
+    post_path = args[1]
     with sync_playwright() as p:
         browser = p.firefox.launch()
         context = browser.new_context(device_scale_factor=2)
@@ -17,10 +18,21 @@ def main(args):
         locator = page.locator("#smallmultiples-container")
         divs = locator.locator("div")
         expect(divs).to_have_count(count=3, timeout=10000)
+        levels = []
         for idx, div in enumerate(divs.all()):
-            p = f"{path}{idx}"
-            div.screenshot(path=p)
-        browser.close()
+            place = div.locator("p.h3").text_content()
+            place = place.removeprefix("Halifax ")
+            level = div.locator("span.badge").text_content()
+            levels.append((place, level))
+            base = f"{post_path}.image-{idx}"
+            div.screenshot(path=base)
+            with open(f"{base}.alt", "w") as f:
+                f.write(f"Chart for {place} as of {date} showing the level of COVID-19 in wastewater as {level}.")
+
+        with open(f"{post_path}", "w") as f:
+            f.write(f"COVID wastewater charts for the Halifax area as of {date}\n\n")
+            for level in levels:
+                f.write(f"{level[0]}: {level[1]}\n")
 
 if __name__ == "__main__":
     main(args=sys.argv[1:])
